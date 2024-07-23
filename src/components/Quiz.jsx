358cb24dfa1c8fc60ee1b18c7df7b788b4ccbe39
khazarglzd <khazarguluzade@gmail.com>
2024-07-23 16:18:35 +0400

    creating Quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import "../App.css";
+import { Questions } from "../helpers/Questions";
+import { useState } from "react";
 
-const Quiz = () => {
+function Quiz() {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+
+
     return (
-        <div>Quiz</div>
-    )
+        <div className="Quiz">
+            <h1>{Questions[currentQuestion].prompt}</h1>
+            <button>{Questions[currentQuestion].optionA}</button>
+            <button>{Questions[currentQuestion].optionB}</button>
+            <button>{Questions[currentQuestion].optionC}</button>
+            <button>{Questions[currentQuestion].optionD}</button>
+        </div>
+    );
 }
 
-export default Quiz
+export default Quiz;
